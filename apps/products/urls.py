9de20422f1ec52b5ce22783a7677c203144498ac
Mohamed Khalil ACHMEROUANINE <mk.achmerouanine@gmail.com>
2022-06-30 14:25:59 +0100

    Working with views & forms

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from .views import product_list
+from .views import add_product, product_list, product_details
+
+app_name = "products"
+
 urlpatterns = [
+    path('products/add/', add_product, name="add_product"),
     path('products/', product_list, name="product_list"),
+    path('products/<id>/', product_details, name="product_details"),
 ]
--- apps/products/views.py
+++ apps/products/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-
-from apps.products.models import Product
+from django.shortcuts import redirect, render
+from .forms import ProductForm
+from .models import Product
 
 def product_list(request):
     prds = Product.objects.all()
@@ -8,3 +8,41 @@ def product_list(request):
         'products': prds
     }
     return render(request, 'products/list.html', context)
+
+def product_details(request, id):
+    prd = Product.objects.filter(id=id).first()
+    context = {
+        'product': prd
+    }
+    return render(request, 'products/details.html', context)
+"""
+d = {
+    "title": 'titre',
+    "object": {
+        "attrs": [1,2,3,4,5,6],
+        'other': {'hack': 'it'}
+    }
+}
+d['object']['other']['hack']
+d.get('object').get('other').get('hack')
+"""
+def add_product(request):
+    form = ProductForm()
+    if request.method == 'POST':
+        form = ProductForm(data=request.POST)
+        if form.is_valid():
+            print(request.POST)
+            form.save()
+            return redirect('products:product_list')
+            """
+            p = Product(
+                title= request.POST.get('title'),
+                price=request.POST['price']
+            )
+            """
+        else:
+            form = ProductForm(data=request.POST)
+    context = {
+        'form': form
+    }
+    return render(request, 'products/add.html', context)
