9b908a4504650f5216b9e020bec2d5a4adacba54
Mohamed Khalil ACHMEROUANINE <mk.achmerouanine@gmail.com>
2022-07-01 16:16:26 +0100

    urls.py

--- core/urls.py
+++ core/urls.py
@@ -13,5 +13,4 @@ urlpatterns = [
     path('login/', login_page),
     path('logout/', logout_page, name="logout"),
     path('products/', include('apps.products.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
